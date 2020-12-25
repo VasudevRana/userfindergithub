b6a5ff8909c6888f04c38ddac6555511a35ca588
VasudevRana <45121999+VasudevRana@users.noreply.github.com>
2020-12-25 22:21:09 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+const url = `https://api.github.com/users`;
+export default function App() {
+  let [s, setS] = React.useState("");
+  let [myresults, setMyresults] = React.useState([]);
+  React.useEffect(function(){
+    let b = []
+    if (localStorage.getItem("results")) {
+      b = JSON.parse(localStorage.getItem("results"));
 
-function App() {
+    }
+    
+    
+    setMyresults(b);
+    
+  },[])
+
+  const _search = e => {
+    
+    axios
+      .get(url + "/" + s)
+      .then(d => d.data)
+      .then(d => {
+        if (d) {
+          let b = []
+          if (localStorage.getItem("results")) {
+            b = JSON.parse(localStorage.getItem("results"));
+
+          }
+          b.push(d);
+          localStorage.setItem("results", JSON.stringify(b));
+          setMyresults(b);
+          setS('');
+          alert("user added");
+        }
+        else{
+          alert("not found")
+        }
+      });
+  };
+  const del = index => {
+    let b = []
+    if (localStorage.getItem("results")) {
+      b = JSON.parse(localStorage.getItem("results"));
+
+    }
+    alert("Confirm delete");
+    b.splice(index, 1)
+    localStorage.setItem("results", JSON.stringify(b));
+    setMyresults(b);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <h1>Github Search</h1>
+      <button className="button" onMouseEnter={_search}>search </button>
+      <input
+      className="input"
+        onChange={e => {setS(e.target.value);}
+        }
+        placeholder="search..."
+        value={s}
+      />
+      <button className="button" onMouseEnter={_search}>search </button>
+      <div className="pre">
+        {myresults.map((x, i) => (
+          <li key={i}>
+
+            UserId:-{x.id} <br></br>
+            Name:-{x.name} <br></br>
+            Link:-{x.html_url} <br></br>
+            Repos:-{x.public_repos} <br></br>
+            followers:-{x.followers} <br></br>
+            following:-{x.following}<br></br>
+            <button className="delete" onClick={e => del(i)}>delete</button>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
